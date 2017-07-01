0eedffe57cb1901caed0aef2502e0d1166ead0c0
LuisGMJ <lg.mj9520@gmail.com>
2017-07-01 02:57:44 -0500

    Validation!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,20 @@
-Product.delete_all
-# . . .
-Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
-                description:
-                    %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
-      </p>},
-                image_url: '7apps.jpg',
-                price: 26.00)
-# . . .
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
